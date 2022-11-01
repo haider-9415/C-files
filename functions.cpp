6944079b2343999275a0ed43dfe4d127b628b837
haider-9415 <116593197+haider-9415@users.noreply.github.com>
2022-11-01 22:11:11 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int add(int a,int b){
+    cout<<a+b;
+}
+
+// we can create multiple functions using same name
+int add(int a,int b,int c,int d){
+    int sum=a+b+c+d;
+    cout<<sum;
+}
+// this function is to find 'a' to the power 'b'
+int power(int a,int b){
+    int x=1;
+    for (int i = 1; i <= b; i++)
+    {
+        x*=a;
+    }
+    cout<<x;   
+}
+
+int factorial(int n){
+    int x=1;
+    for (int i = 1; i <= n; i++)
+    {
+        x*=i;
+    }
+    cout<<x;
+}
+string print(string x){
+    cout<<x;
+}
+int main(){
+    add(4,5);
+    print("\n");
+
+    int c=10,d=20;
+    add(c,d);
+    print("\n");
+
+    add(10,20,30,40);
+    print("\n");
+
+    power(2,4);
+    print("\n");
+
+    factorial(5);
+
+
+}
