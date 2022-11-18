4d9f4bf7724e454df6f23c70d179bf6650eba538
haider-9415 <116593197+haider-9415@users.noreply.github.com>
2022-11-18 10:00:27 +0530

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -55,20 +55,64 @@ int odd(int a,int b){
 
 
 
-/* To find all prime numbers between a and b */
+/* To find all prime numbers between 'a' and 'b' */
+
+int prime(int a , int b ){
+    cout<<"All prime nubers between "<<a<<" and "<<b<<": ";
+    for (int i = a+1 ; i < b; i++)
+    {
+        // to print 2 if 'a = 1'
+        if (i==2)
+        {
+            cout<<2<<",";
+        }
+        // to extract all odd numbers, because, all prime numbers are odd numbers and 2 is the only even number that is prime number
+        if (i%2 != 0)
+        {
+            // if 'x' is a prime number then 'x%1==0' and 'x%X==0', therefor, 'j=2' and 'j<i'
+            int j = 2;
+            for (; j < i; j++)
+            {
+                // to check that 'i' is divisible by 1 and itself only
+                if (i%j != 0)
+                {
+                    continue;
+                }
+                // if 'i' is not divisible by 1 and itself only
+                break;
+
+            }
+            // to print 'i' if it is a prime number
+            if (j == i)
+            {
+                cout<<i<<",";
+            }
+        }   
+    }
+    cout<<endl;
+}
 
 
 
 int main(){
     square(10);
+    cout<<endl;
 
     radius(3);
+    cout<<endl;
 
     age(12);
     age(20);
+    cout<<endl;
 
     odd(1,10);
     odd(100,200);
+    cout<<endl;
+    
+    prime(1,100);
+    prime(2,50);
+    prime(11,39);
+    cout<<endl;
 
 
 }
